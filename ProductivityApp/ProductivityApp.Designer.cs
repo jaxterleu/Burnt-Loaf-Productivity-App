680c2eb78c5f77a812f3d91d707d20a4446da2ba
Habibread <jaimearuego@gmail.com>
2025-04-25 21:30:17 +0800

    All Tools Added
    Each tool was added and arranged into the list

--- ProductivityApp/ProductivityApp.Designer.cs
+++ ProductivityApp/ProductivityApp.Designer.cs
@@ -42,6 +42,16 @@
             btnReset = new Button();
             numCycles = new NumericUpDown();
             lblCycles = new Label();
+            lblTaskList = new Label();
+            clbTaskList = new CheckedListBox();
+            lblFinTasks = new Label();
+            lbxFinTasks = new ListBox();
+            btnAdd = new Button();
+            btnEdit = new Button();
+            btnDelete = new Button();
+            btnUpdate = new Button();
+            btnRemove = new Button();
+            btnFinish = new Button();
             ((System.ComponentModel.ISupportInitialize)numWork).BeginInit();
             ((System.ComponentModel.ISupportInitialize)numShortBreak).BeginInit();
             ((System.ComponentModel.ISupportInitialize)numLongBreak).BeginInit();
@@ -52,18 +62,18 @@
             // 
             lblTime.AutoSize = true;
             lblTime.Font = new Font("Segoe UI", 24F);
-            lblTime.Location = new Point(52, 75);
+            lblTime.Location = new Point(59, 100);
             lblTime.Name = "lblTime";
-            lblTime.Size = new Size(95, 45);
+            lblTime.Size = new Size(120, 54);
             lblTime.TabIndex = 0;
             lblTime.Text = "00:00";
             // 
             // lblStatus
             // 
             lblStatus.AutoSize = true;
-            lblStatus.Location = new Point(84, 120);
+            lblStatus.Location = new Point(96, 160);
             lblStatus.Name = "lblStatus";
-            lblStatus.Size = new Size(26, 15);
+            lblStatus.Size = new Size(34, 20);
             lblStatus.TabIndex = 1;
             lblStatus.Text = "Idle";
             lblStatus.TextAlign = ContentAlignment.MiddleCenter;
@@ -71,36 +81,39 @@
             // lblSessions
             // 
             lblSessions.AutoSize = true;
-            lblSessions.Location = new Point(127, 179);
+            lblSessions.Location = new Point(145, 239);
             lblSessions.Name = "lblSessions";
-            lblSessions.Size = new Size(63, 15);
+            lblSessions.Size = new Size(79, 20);
             lblSessions.TabIndex = 2;
             lblSessions.Text = "Sessions: 0";
             lblSessions.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // numWork
             // 
-            numWork.Location = new Point(181, 214);
+            numWork.Location = new Point(207, 285);
+            numWork.Margin = new Padding(3, 4, 3, 4);
             numWork.Name = "numWork";
-            numWork.Size = new Size(46, 23);
+            numWork.Size = new Size(53, 27);
             numWork.TabIndex = 3;
             numWork.TextAlign = HorizontalAlignment.Center;
             numWork.Value = new decimal(new int[] { 25, 0, 0, 0 });
             // 
             // numShortBreak
             // 
-            numShortBreak.Location = new Point(181, 243);
+            numShortBreak.Location = new Point(207, 324);
+            numShortBreak.Margin = new Padding(3, 4, 3, 4);
             numShortBreak.Name = "numShortBreak";
-            numShortBreak.Size = new Size(46, 23);
+            numShortBreak.Size = new Size(53, 27);
             numShortBreak.TabIndex = 4;
             numShortBreak.TextAlign = HorizontalAlignment.Center;
             numShortBreak.Value = new decimal(new int[] { 5, 0, 0, 0 });
             // 
             // numLongBreak
             // 
-            numLongBreak.Location = new Point(181, 272);
+            numLongBreak.Location = new Point(207, 363);
+            numLongBreak.Margin = new Padding(3, 4, 3, 4);
             numLongBreak.Name = "numLongBreak";
-            numLongBreak.Size = new Size(46, 23);
+            numLongBreak.Size = new Size(53, 27);
             numLongBreak.TabIndex = 5;
             numLongBreak.TextAlign = HorizontalAlignment.Center;
             numLongBreak.Value = new decimal(new int[] { 15, 0, 0, 0 });
@@ -108,35 +121,36 @@
             // lblWork
             // 
             lblWork.AutoSize = true;
-            lblWork.Location = new Point(104, 216);
+            lblWork.Location = new Point(119, 288);
             lblWork.Name = "lblWork";
-            lblWork.Size = new Size(35, 15);
+            lblWork.Size = new Size(43, 20);
             lblWork.TabIndex = 6;
             lblWork.Text = "Work";
             // 
             // lblShortBreak
             // 
             lblShortBreak.AutoSize = true;
-            lblShortBreak.Location = new Point(103, 247);
+            lblShortBreak.Location = new Point(118, 329);
             lblShortBreak.Name = "lblShortBreak";
-            lblShortBreak.Size = new Size(67, 15);
+            lblShortBreak.Size = new Size(85, 20);
             lblShortBreak.TabIndex = 7;
             lblShortBreak.Text = "Short Break";
             // 
             // lblLongBreak
             // 
             lblLongBreak.AutoSize = true;
-            lblLongBreak.Location = new Point(103, 276);
+            lblLongBreak.Location = new Point(118, 368);
             lblLongBreak.Name = "lblLongBreak";
-            lblLongBreak.Size = new Size(66, 15);
+            lblLongBreak.Size = new Size(83, 20);
             lblLongBreak.TabIndex = 8;
             lblLongBreak.Text = "Long Break";
             // 
             // btnStart
             // 
-            btnStart.Location = new Point(212, 67);
+            btnStart.Location = new Point(242, 89);
+            btnStart.Margin = new Padding(3, 4, 3, 4);
             btnStart.Name = "btnStart";
-            btnStart.Size = new Size(75, 23);
+            btnStart.Size = new Size(86, 31);
             btnStart.TabIndex = 9;
             btnStart.Text = "Start";
             btnStart.UseVisualStyleBackColor = true;
@@ -144,9 +158,10 @@
             // 
             // btnPause
             // 
-            btnPause.Location = new Point(212, 96);
+            btnPause.Location = new Point(242, 128);
+            btnPause.Margin = new Padding(3, 4, 3, 4);
             btnPause.Name = "btnPause";
-            btnPause.Size = new Size(75, 23);
+            btnPause.Size = new Size(86, 31);
             btnPause.TabIndex = 10;
             btnPause.Text = "Pause";
             btnPause.UseVisualStyleBackColor = true;
@@ -154,9 +169,10 @@
             // 
             // btnReset
             // 
-            btnReset.Location = new Point(212, 125);
+            btnReset.Location = new Point(242, 167);
+            btnReset.Margin = new Padding(3, 4, 3, 4);
             btnReset.Name = "btnReset";
-            btnReset.Size = new Size(75, 23);
+            btnReset.Size = new Size(86, 31);
             btnReset.TabIndex = 11;
             btnReset.Text = "Reset";
             btnReset.UseVisualStyleBackColor = true;
@@ -164,11 +180,12 @@
             // 
             // numCycles
             // 
-            numCycles.Location = new Point(181, 301);
+            numCycles.Location = new Point(207, 401);
+            numCycles.Margin = new Padding(3, 4, 3, 4);
             numCycles.Maximum = new decimal(new int[] { 10, 0, 0, 0 });
             numCycles.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
             numCycles.Name = "numCycles";
-            numCycles.Size = new Size(46, 23);
+            numCycles.Size = new Size(53, 27);
             numCycles.TabIndex = 12;
             numCycles.TextAlign = HorizontalAlignment.Center;
             numCycles.Value = new decimal(new int[] { 1, 0, 0, 0 });
@@ -176,17 +193,115 @@
             // lblCycles
             // 
             lblCycles.AutoSize = true;
-            lblCycles.Location = new Point(104, 305);
+            lblCycles.Location = new Point(119, 407);
             lblCycles.Name = "lblCycles";
-            lblCycles.Size = new Size(41, 15);
+            lblCycles.Size = new Size(50, 20);
             lblCycles.TabIndex = 13;
             lblCycles.Text = "Cycles";
             // 
+            // lblTaskList
+            // 
+            lblTaskList.AutoSize = true;
+            lblTaskList.Location = new Point(343, 9);
+            lblTaskList.Name = "lblTaskList";
+            lblTaskList.Size = new Size(71, 20);
+            lblTaskList.TabIndex = 14;
+            lblTaskList.Text = "Tasks List:";
+            // 
+            // clbTaskList
+            // 
+            clbTaskList.FormattingEnabled = true;
+            clbTaskList.Location = new Point(343, 32);
+            clbTaskList.Name = "clbTaskList";
+            clbTaskList.Size = new Size(291, 466);
+            clbTaskList.TabIndex = 15;
+            // 
+            // lblFinTasks
+            // 
+            lblFinTasks.AutoSize = true;
+            lblFinTasks.Location = new Point(657, 9);
+            lblFinTasks.Name = "lblFinTasks";
+            lblFinTasks.Size = new Size(103, 20);
+            lblFinTasks.TabIndex = 16;
+            lblFinTasks.Text = "Finished Tasks:";
+            // 
+            // lbxFinTasks
+            // 
+            lbxFinTasks.FormattingEnabled = true;
+            lbxFinTasks.Location = new Point(657, 32);
+            lbxFinTasks.Name = "lbxFinTasks";
+            lbxFinTasks.Size = new Size(245, 470);
+            lbxFinTasks.TabIndex = 17;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(343, 513);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(94, 29);
+            btnAdd.TabIndex = 18;
+            btnAdd.Text = "Add New";
+            btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            btnEdit.Location = new Point(443, 513);
+            btnEdit.Name = "btnEdit";
+            btnEdit.Size = new Size(94, 29);
+            btnEdit.TabIndex = 19;
+            btnEdit.Text = "Edit Task";
+            btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(543, 513);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(94, 29);
+            btnDelete.TabIndex = 20;
+            btnDelete.Text = "Delete Task";
+            btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(657, 515);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(117, 29);
+            btnUpdate.TabIndex = 21;
+            btnUpdate.Text = "Update List";
+            btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnRemove
+            // 
+            btnRemove.Location = new Point(785, 515);
+            btnRemove.Name = "btnRemove";
+            btnRemove.Size = new Size(117, 29);
+            btnRemove.TabIndex = 22;
+            btnRemove.Text = "Remove Task";
+            btnRemove.UseVisualStyleBackColor = true;
+            // 
+            // btnFinish
+            // 
+            btnFinish.Location = new Point(338, 550);
+            btnFinish.Name = "btnFinish";
+            btnFinish.Size = new Size(564, 29);
+            btnFinish.TabIndex = 23;
+            btnFinish.Text = "Finish Session";
+            btnFinish.UseVisualStyleBackColor = true;
+            // 
             // ProductivityApp
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(914, 600);
+            Controls.Add(btnFinish);
+            Controls.Add(btnRemove);
+            Controls.Add(btnUpdate);
+            Controls.Add(btnDelete);
+            Controls.Add(btnEdit);
+            Controls.Add(btnAdd);
+            Controls.Add(lbxFinTasks);
+            Controls.Add(lblFinTasks);
+            Controls.Add(clbTaskList);
+            Controls.Add(lblTaskList);
             Controls.Add(lblCycles);
             Controls.Add(numCycles);
             Controls.Add(btnReset);
@@ -201,6 +316,7 @@
             Controls.Add(lblSessions);
             Controls.Add(lblStatus);
             Controls.Add(lblTime);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "ProductivityApp";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)numWork).EndInit();
@@ -227,5 +343,15 @@
         private Button btnReset;
         private NumericUpDown numCycles;
         private Label lblCycles;
+        private Label lblTaskList;
+        private CheckedListBox clbTaskList;
+        private Label lblFinTasks;
+        private ListBox lbxFinTasks;
+        private Button btnAdd;
+        private Button btnEdit;
+        private Button btnDelete;
+        private Button btnUpdate;
+        private Button btnRemove;
+        private Button btnFinish;
     }
 }
